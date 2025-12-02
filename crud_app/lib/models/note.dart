e308370619863f02ac36e331b144fd27c2789d64
sharulariffin <sharul.ariffin13@gmail.com>
2025-12-03 01:41:22 +0800

    feat: add Note model and update dependencies

---
+++ crud_app/lib/models/note.dart
@@ -0,0 +1,25 @@
+class Note {
+    final String id;
+    final String title;
+    final String content;
+    final DateTime createdAt;
+
+    Note({
+        required this.id,
+        required this.title,
+        required this.content,
+        required this.createdAt,
+    });
+
+    factory Note.fromJson(Map<String, dynamic> json) {
+        return Note(
+            id: json['id'],
+            title: json['title'],
+            content: json['content'],
+            createdAt: DateTime.parse(json['createdAt']),
+        );
+    }
+}
+
+
+
