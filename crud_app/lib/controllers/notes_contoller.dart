05287d2c71df48f59ad3344087ddaf608f5973da
sharulariffin <sharul.ariffin13@gmail.com>
2025-12-20 01:35:49 +0800

    feat: enhance NoteController with loading state and error handling for note fetching

--- crud_app/lib/controllers/notes_contoller.dart
+++ crud_app/lib/controllers/notes_contoller.dart
@@ -3,5 +3,45 @@ import '../models/note.dart';
 import '../services/api_service.dart';
 
 class NoteController extends ChangeNotifier {
+    // public state variables
     List<Note> _notes = [];
+    bool _isLoading  = false;
+    String? _error;
+
+    // public getters: prevents --> accidentally data changes, impossible-to-debug bugs
+    List<Note> get notes => _notes;
+    bool get isLoading => _isLoading;
+    string? get error => _error;
+
+    // function to fetch all notes
+    Future<void> fetchNotes() async {
+        _isLoading = true;  // marks loading state
+        _error = null;      // clear previous errors
+        notifyListeners();  // tells UI --> to rebuild now
+
+        try {
+            _notes = await ApiServices.fetchAllNotes();
+        } catch (e) {
+            _error = e.toString();
+        } finally {
+            _isLoading = false;
+            notifyListeners();
+        }
+    }
+
+    // when createNote() run, fetchNotes runs after it succeeds
+    Future<void> createNote(String title, String content) async {
+        await ApiService.createNote(title, content);
+        await fetchNotes();
+    }
+
+    Future<void> updateNote(String id, String title, String content) async {
+        await ApiService.updateNote(id, title, content);
+        await fetchNotes();
+    }
+
+    Future<void> deleteNote(String id) async {
+        await ApiService.deleteNote(id);
+        await fetchNotes();
+    }
 }
